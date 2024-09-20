cdf7c8b1a1a86f62b23a4603aa045c5f79d6ea83
lucholopez1 <lulopezro@unal.edu.co>
2024-09-19 19:03:09 -0500

    Update base_de_datos.py

--- proyecto de programacion/src/base_de_datos.py
+++ proyecto de programacion/src/base_de_datos.py
@@ -54,11 +54,10 @@ def agregar_seleccion():
     #se llama a la funcion de busqueda para saber si ese codigo ya esta en la base de datos
     
     codigo_encontrado, _, _, _=buscar_codigo()
- 
-    
+
     #se crea la ventana para especificar el numero de unidades
     interfaz_numerica = tk.Tk()
-    interfaz_numerica.title("Seleccionar valor")
+    interfaz_numerica.title("Seleccionar Valor")
     interfaz_numerica.geometry("200x100")
     
     #esta funcion retorna el valor numerico que el usuario escogio
@@ -71,7 +70,7 @@ def agregar_seleccion():
     
     def agregar():  
         #en caso de estar repetido se desplegara una ventana donde se podra especificar el numero de unidades que se desea agrega
-        if codigo_encontrado==True:
+        if codigo_encontrado == True:
             #todo esto modifica el valor de la columna de cantidad del codigo que se elijio deacuerdo al numero que elijio el usuario
             _, posiciones, cantidad, _=buscar_codigo() # se llaman los datos que se obtuvieron de la funcion de busqueda
             archivo = pd.read_csv('base_de_datos.csv', delimiter=';') #se lee la base datos
@@ -86,8 +85,7 @@ def agregar_seleccion():
             "CANTIDAD": nueva_cantidad
             }
             archivo.to_csv('base_de_datos.csv', index=False, sep=';') # se guarda la modificacion
-            interfaz_numerica.destroy() #se destruye la mini interfaz
-        
+            interfaz_numerica.destroy() #se destruye la mini interfaz
         
         #en caso no estar en la base de datos se agregara un valor nueco a la base de datos    
         else:
@@ -106,6 +104,7 @@ def agregar_seleccion():
             guardar_selecciones()
             datos.clear()
             interfaz_numerica.destroy()
+
     boton = tk.Button(interfaz_numerica, text="Confirmar", command=agregar)
     boton.pack(pady=10)
     interfaz_numerica.mainloop()
@@ -116,17 +115,18 @@ def agregar_seleccion():
 #esta funcion se encarga de darle al usuario los datos acerca de la busqueda de un codgio en especifico    
 
 def filtardo():
-    codigo_encontrado, _, cantidad, codigo_buscado=buscar_codigo() #se llaman los datos de la funcion de busqeuda
+    codigo_encontrado, _, cantidad, codigo_buscado = buscar_codigo() #se llaman los datos de la funcion de busqeuda
     #en caso de ser encontrado se le mostrar en pantalla el codigo de su producto y el numero de unidades en stock
     if codigo_encontrado==True:
-        messagebox.showinfo("busqueda en la base de datos", f"el producto se representa con el codigo {codigo_buscado} y se encuentran en stock {cantidad} unidades")
+        messagebox.showinfo("Búsqueda en la base de datos", f"El producto se representa con el codigo {codigo_buscado} y se encuentran en stock {cantidad} unidades.")
     #en caso de no ser encontrada en la base datos se le informara que no fue encontrada y su respectivo codigo
     else:
-         messagebox.showinfo("error en busqueda", f"el producto de codigo {codigo_buscado} no se encuentra en la base de datos ")
+         messagebox.showinfo("Error en busqueda", f"El producto de codigo {codigo_buscado} no se encuentra en la base de datos.")
 
 #esta funcion sirve para guardar los datos recogidos en la funcion de gregar y adicionarlos o reemplazarlos en la base de datos
 
 def guardar_selecciones():
+
     if datos: 
     
         df = pd.DataFrame(datos)
@@ -146,13 +146,13 @@ def eliminar():
     if codigo_encontrado==True: 
         #se crea una interfaz para dar la opcion
         mini_interfaz = tk.Tk()
-        mini_interfaz.title("seleccione")
+        mini_interfaz.title("Seleccione: ")
         #en caso de elegir el reseteo esta funcion con el dato de posicion eliminara esa fila
         def reseteo():
             archivo = pd.read_csv('base_de_datos.csv', delimiter=';')
             archivo=archivo.drop(posiciones[0][0])
             archivo.to_csv('base_de_datos.csv', index=False, sep=';')
-            messagebox.showinfo("exito","se reseteo el producto con exito")
+            messagebox.showinfo("Exito","Se reseteó el producto con exito")
             mini_interfaz.destroy()
             
         #en caso de elegir eliminar por unidades se le preguntara por cuantas unidades desea restar y lo hara esta funcion
@@ -169,7 +169,7 @@ def eliminar():
                 _, posiciones, cantidad, _=buscar_codigo()
                 archivo = pd.read_csv('base_de_datos.csv', delimiter=';')
                 nueva_cantidad = cantidad-int(numero())
-                codigo=generar_id(respuesta_opcion_marca.get().strip(), respuesta_opcion_almacenamiento.get().strip(), respuesta_opcion_modelo.get().strip())
+                codigo = generar_id(respuesta_opcion_marca.get().strip(), respuesta_opcion_almacenamiento.get().strip(), respuesta_opcion_modelo.get().strip())
                 archivo.loc[posiciones[0][0]] ={
                 "MARCA": respuesta_opcion_marca.get(),
                 "ALMACENAMIENTO": respuesta_opcion_almacenamiento.get(),
@@ -178,7 +178,7 @@ def eliminar():
                 "CANTIDAD": nueva_cantidad
             }
                 archivo.to_csv('base_de_datos.csv', index=False, sep=';')
-                messagebox.showinfo("exito", "se elimino la unidad exitosamente")
+                messagebox.showinfo("Exito", "Se elimino la unidad exitosamente")
                 unidades_restar.destroy()
                 mini_interfaz.destroy()
                                                   #parametros de las interfaz    
@@ -187,20 +187,20 @@ def eliminar():
             boton = tk.Button(unidades_restar, text="Confirmar", command=eliminar_unidad)
             boton.pack(pady=10)
             unidades_restar.mainloop()            
-        boton1 = tk.Button(mini_interfaz, text="resetear", command=reseteo, bg="lightblue")
-        boton2 = tk.Button(mini_interfaz, text="eliminar unidades", command=elemento, bg="lightgreen")
+        boton1 = tk.Button(mini_interfaz, text="Resetear", command=reseteo, bg="lightblue")
+        boton2 = tk.Button(mini_interfaz, text="Elminar Unidades", command=elemento, bg="lightgreen")
         boton1.grid(row=0, column=0, padx=10, pady=10)
         boton2.grid(row=0, column=1, padx=10, pady=10)
         mini_interfaz.mainloop()
     # en caso de encontrarse el codigo se le informara al usuario
     else:
-        messagebox.showinfo("error", "el producto que desea eliminar nose encuentra en la base de datos")
+        messagebox.showinfo("Error", "El producto que desea eliminar no se encuentra en la base de datos")
     
 # esta funcion le mostrar la tabla de datos al usuario
 def abrir_archivo():
-    root=tk.Tk()
+    root = tk.Tk()
     archivo = pd.read_csv('base_de_datos.csv', delimiter=';') 
-    X= archivo.to_string(index=False, header=True, col_space=10)
+    X = archivo.to_string(index=False, header=True, col_space=10)
     text_widget = ScrolledText(root, width=65, height=30)
     text_widget.pack(pady=10)
     text_widget.insert(tk.END, X)
@@ -208,13 +208,13 @@ def abrir_archivo():
     
 #funcion para cerrar la interfaz    
 def cerrar():
-    interfaz.destroy() #destruye la interfaz
+    interfaz.destroy() #destruye la interfaz    
 
 # Crear la ventana principal
 interfaz = tk.Tk()
 interfaz.title("Interfaz de la Base de Datos")
 interfaz.geometry("500x400")
-interfaz.configure(bg="#f0f0f0")  # Fondo claro
+interfaz.configure(bg="ivory3")  # Fondo 
 
 # Etiquetas y menús desplegables para seleccionar opciones
 titulo_label = tk.Label(interfaz, text="Gestión de Base de Datos",bg="#003366", fg="white" , font=("Arial", 16, "bold"))
@@ -225,21 +225,21 @@ tk.Label(interfaz, text="Seleccione la marca del dispositivo:", bg="#f0f0f0").gr
 respuesta_opcion_marca = tk.StringVar()
 marca_combobox = ttk.Combobox(interfaz, textvariable=respuesta_opcion_marca, values=["Apple", "Samsung", "Xiaomi"], state="readonly")
 marca_combobox.grid(row=1, column=1, padx=20, pady=5, sticky="e")
-marca_combobox.set("Seleccione marca")
+marca_combobox.set("Seleccione Marca")
 
 # Almacenamiento
 tk.Label(interfaz, text="Seleccione el almacenamiento del dispositivo:", bg="#f0f0f0").grid(row=2, column=0, padx=20, pady=5, sticky="w")
 respuesta_opcion_almacenamiento = tk.StringVar()
 almacenamiento_combobox = ttk.Combobox(interfaz, textvariable=respuesta_opcion_almacenamiento, values=["64GB", "128GB", "256GB"], state="readonly")
 almacenamiento_combobox.grid(row=2, column=1, padx=20, pady=5, sticky="e")
-almacenamiento_combobox.set("Seleccione almacenamiento")
+almacenamiento_combobox.set("Seleccione Almacenamiento")
 
 # Modelo
 tk.Label(interfaz, text="Seleccione el rango de fechas del dispositivo:", bg="#f0f0f0").grid(row=3, column=0, padx=20, pady=5, sticky="w")
 respuesta_opcion_modelo = tk.StringVar()
 modelo_combobox = ttk.Combobox(interfaz, textvariable=respuesta_opcion_modelo, values=["2022/2024", "2019/2021", "2016/2018"], state="readonly")
 modelo_combobox.grid(row=3, column=1, padx=20, pady=5, sticky="e")
-modelo_combobox.set("Seleccione modelo")
+modelo_combobox.set("Seleccione Modelo")
 
 # Botones de acción
 frame_botones = tk.Frame(interfaz, bg="#f0f0f0")
@@ -249,15 +249,6 @@ tk.Button(frame_botones, text="Agregar selección", command=agregar_seleccion, b
 tk.Button(frame_botones, text="Buscar en base de datos", command=filtardo, bg="tomato2", fg="white", width=20).grid(row=0, column=1, padx=10, pady=5)
 tk.Button(frame_botones, text="Eliminar", command=eliminar, bg="tomato2", fg="white", width=20).grid(row=1, column=0, padx=10, pady=5)
 tk.Button(frame_botones, text="Abrir archivo", command=abrir_archivo, bg="tomato2", fg="white", width=20).grid(row=1, column=1, padx=10, pady=5)
-
-# Menú superior
-menu_bar = Menu(interfaz)
-opciones_menu = Menu(menu_bar, tearoff=0)
-menu_bar.add_cascade(label="Opciones", menu=opciones_menu)
-opciones_menu.add_command(label="Guardar", command=guardar_selecciones)
-opciones_menu.add_command(label="Salir", command=cerrar)
-interfaz.config(menu=menu_bar)
-
 
 def mostrar_grafico(tipo):
     try:
